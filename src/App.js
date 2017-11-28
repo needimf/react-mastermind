3f64a0a2180407919080109fc1cb414a5c92d912
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-11-28 12:33:12 -0800

    Created GameBoard component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import GameBoard from './components/GameBoard/GameBoard';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">React Mastermind</header>
+        <GameBoard />
       </div>
     );
   }
