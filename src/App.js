58503b0e7cf90569b4e59632a7cae1a0ae75d7da
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-11-29 21:08:04 -0800

    Built out state, and win logic on App.js.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,50 @@ import ColorPicker from './components/ColorPicker/ColorPicker';
 import NewGameButton from './components/NewGameButton/NewGameButton';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    let colors = ["#7DBEA5", "#F1E0B1", "#EE9D31", "#F26C1A", "#5A392B"];
+    this.state = {
+      colors,
+      code: this.genCode(colors.length),
+      selColorIdx: 0,
+      guesses: [this.getNewGuess()]
+    };
+  }
+
+  getNewGuess() {
+    return {
+      // code: [null, null, null, null],
+      code: [3, 2, 1, 0], // for testing purposes
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    };
+  }
+
+  genCode(size) {
+    return new Array(size).fill().map(dummy => Math.floor(Math.random() * size));
+  }
+
+  getWinTries() {
+    // if winner, return number of guesses, otherwise 0 (no winner)
+    let lastGuess = this.state.guesses.length - 1;
+    return this.state.code.join() === this.state.guesses[lastGuess].join() ? lastGuess + 1 : 0;
+  }
+
   render() {
+    let winTries = this.getWinTries();
     return (
       <div className="App">
         <header className="App-header">React Mastermind</header>
-        <GameBoard />
-        <br/>
-        <ColorPicker />
+        <GameBoard
+          guesses={this.state.guesses}
+          colors={this.state.colors} 
+        />
+        <ColorPicker colors={this.state.colors} />
         <NewGameButton />
+        <footer>{(winTries ? `You Won in ${winTries} Guesses!` : 'Good Luck!')}</footer>
       </div>
     );
   }
